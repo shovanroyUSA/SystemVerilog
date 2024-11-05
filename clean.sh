cbb9ce4688b483b4a133c5f45904fe4a6bbeb80b
Shovan Roy <shovan_bd@yahoo.com>
2024-11-05 11:38:50 -0800

    Create clean.sh
    script for cleanup

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Clean up previous build files
+rm -rf ./AN.DB ./csrc ./DVEfiles ./simv ./simv.daidir ./ucli.key ./vc_hdrs.h dump.vcd simulation.log eth_pkt_waveform.vpd vcdplus.vpd *.tcl *.log *.old
