3edcad6207b4cffbd56903f25a70153684cf596c
Shovan Roy <shovan_bd@yahoo.com>
2024-11-14 20:17:13 -0800

    Create intf.sv

---
+++ monitor/intf.sv
@@ -0,0 +1,15 @@
+```systemverilog
+interface drv_intf(input logic clk);
+  logic ready; 
+  logic  sop;
+  logic valid;
+  logic [63:0] data;
+  logic        eop;
+  logic [6:0]  bc;
+  //create modports
+  modport mport_drv(output sop, eop, valid, data, bc);
+  modport m_monitor (input data, valid, ready, sop, eop, bc);
+  modport m_dut (output ready, input data, valid, sop, eop, bc);
+  
+endinterface // drv_intf
+```
