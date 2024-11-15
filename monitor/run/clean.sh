6ec284eb6e8a43bf6c45981976aa9d66db89e2b8
Shovan Roy <shovan_bd@yahoo.com>
2024-11-14 20:25:06 -0800

    Create clean.sh

---
+++ monitor/run/clean.sh
@@ -0,0 +1,6 @@
+```sh
+#!/bin/bash
+
+# Clean up previous build files
+rm -rf ./AN.DB ./csrc ./local_cache ./DVEfiles ./simv ./simv.daidir ./ucli.key ./vc_hdrs.h dump.vcd simulation.log eth_pkt_waveform.vpd vcdplus.vpd *.tcl *.log *.old
+```
