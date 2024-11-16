0bcc7ee18cbf2bc6eb0d2c0e77264cfe6adfe512
Shovan Roy <shovan_bd@yahoo.com>
2024-11-15 16:11:26 -0800

    Update intf.sv

--- monitor/intf.sv
+++ monitor/intf.sv
@@ -8,8 +8,8 @@ interface drv_intf(input logic clk);
   logic [6:0]  bc;
   //create modports
   modport mport_drv(output sop, eop, valid, data, bc);
-  modport m_monitor (input data, valid, ready, sop, eop, bc);
-  modport m_dut (output ready, input data, valid, sop, eop, bc);
+  //modport m_monitor (input data, valid, ready, sop, eop, bc);
+  modport mport_dut (output ready, input data, valid, sop, eop, bc);
   
 endinterface // drv_intf
 ```
